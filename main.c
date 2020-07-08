acb2cb78b871b8c6be1bd26685ebcb5b1e9f2547
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-07-07 21:35:01 -0400

    reading ptr move, increment and output instructions

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
 	char c;
 
 	char mem[M_SIZE]; // 30k memory cells
+	int ptr = 0;
 	for(int i=0;i<M_SIZE;i++)mem[i]=0;
 
 	// parse file
@@ -27,9 +28,38 @@ int main(int argc, char *argv[])
 		c = fgetc(fp);
 		if(feof(fp)) break;
 
-		printf("%c",c);
+		switch(c)
+		{
+			case '>':
+				ptr++;
+			break;
 
-		
+			case '<':
+				ptr--;
+			break;
+
+			case '+':
+				mem[ptr]++;
+			break;
+
+			case '-':
+				mem[ptr]--;
+			break;
+
+			case '.':
+				printf("%c",mem[ptr]);
+			break;
+
+			case ',':
+			break;
+
+			case '[':
+			break;
+
+			case ']':
+			break;
+
+		}
 	} while(1);
 
 	printf("\n");
