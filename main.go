512c791b36f88976646d525c246fd9c72ae2d78f
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-08-25 20:50:48 -0400

    successfully reading all bf instructions except input (,) ; added hello_world bf code

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -22,13 +21,86 @@ func main() {
 
 	const MEM_SIZE = 30000
 	var mem [MEM_SIZE]byte
+	var ptr uint16 // mem ptr
 
-	fmt.Println(mem)
+	buf := make([]byte, 1) // file data buffer
 
-	c := make([]byte, 1)
+	bracketCount := 0 // count brackets to find open/closing brackets
 
 	for err == nil {
-		_, err = fp.Read(c)
-		fmt.Printf(strings.ToUpper(string(c)))
-	}
+		_, err = fp.Read(buf)
+
+		switch buf[0] {
+		case '>': // inc mem ptr
+			ptr = (ptr + 1) % MEM_SIZE
+
+		case '<': // dec mem ptr
+			if ptr == 0 {
+				ptr = MEM_SIZE - 1
+			} else {
+				ptr--
+			}
+
+		case '+': // inc cell
+			mem[ptr]++
+
+		case '-': // dec cell
+			mem[ptr]--
+
+		case '.': // output
+			fmt.Printf("%c", mem[ptr])
+
+		case '[':
+			if mem[ptr] != 0 {
+				break
+			}
+
+			bracketCount = 1
+
+			for bracketCount != 0 {
+				_, err = fp.Read(buf)
+
+				if err != nil {
+					fmt.Println("Missing ']'.")
+					return
+				}
+
+				if buf[0] == '[' {
+					bracketCount++
+				} else if buf[0] == ']' {
+					bracketCount--
+				}
+
+			}
+
+		case ']':
+			if mem[ptr] == 0 {
+				break
+			}
+
+			bracketCount = 1
+
+			for bracketCount != 0 {
+				// _, err = fp.Read(buf)
+				fp.Seek(-2, 1)
+
+				if err != nil {
+					fmt.Println("Missing '['.")
+					return
+				}
+
+				_, err = fp.Read(buf)
+				if buf[0] == '[' {
+					bracketCount--
+				}
+				if buf[0] == ']' {
+					bracketCount++
+				}
+			}
+
+			// NEED: input
+
+		} // end switch
+	} // end loop
+
 }
